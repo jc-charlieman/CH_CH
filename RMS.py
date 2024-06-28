885effc92c31f48bba0164a77244237a6fc25616
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-28 16:43:39 -0600

    Update RMS.py

--- RMS.py
+++ RMS.py
@@ -9,21 +9,10 @@ st.title("CHILAQUIL CHILANGO by Wisho!.")
 # Subtitulo
 st.subheader("Menú:")
 
+
+
 # Comida
-
-
-# col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
-# with col1:
-#   i1 = st.number_input("Ch-1:", value=0, min_value=0)
-# with col2:
-#   i2 = st.number_input("Ch-2", value=0)
-# with col3:
-#   i3 = st.number_input("Ch-3", value=0)
-# with col4:
-#   i4 = st.number_input("Ch-4", value=0)
-
-# chilaquiles = 2
-c1, c2 = st.columns([1, 2])
+c1, c2 = st.columns([1, 4])
 with c1:
   st.write("\n")
   st.write("\n")
