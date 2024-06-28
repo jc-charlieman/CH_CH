46564cd4e9e9b9cc7ae44e536627eec519242283
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-28 17:49:34 -0600

    Update RMS.py

--- RMS.py
+++ RMS.py
@@ -17,9 +17,10 @@ with c2:
   chilaquiles = st.number_input("\# Chilaquiles:", min_value=0, value=0)
 
 total = 0
-
-for n in range(1, chilaquiles + 1):
-  st.subheader(f"Chilaquil-{n}")
+# contenedores
+plato, torta, jamon, manchego, huevo = [], [], [], [], []
+for n in range(chilaquiles):
+  st.subheader(f"Chilaquil-{n + 1}")
   c3, c4, c5, c6, c7 = st.columns([1, 1, 1, 1, 1])
   with c3:
     plato = st.number_input("Plato:", min_value=0, value=0)
