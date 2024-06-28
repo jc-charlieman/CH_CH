3b2593bb7a59ca22d85edf8da0b57ddf154454d7
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-28 17:38:51 -0600

    Update RMS.py

--- RMS.py
+++ RMS.py
@@ -18,19 +18,19 @@ with c2:
 
 total = 0
 
-# for chilaquil in chilaquiles:
-st.header(f"{chilaquil}")
-c3, c4, c5, c6, c7 = st.columns([1, 1, 1, 1, 1])
-with c3:
-  pedido = st.number_input("Plato:", min_value=0, value=0)
-with c4:
-  chilaquiles = st.number_input("Torta:", min_value=0, value=0)
-with c5:
-  chilaquiles = st.number_input("Jamón:", min_value=0, value=0)
-with c6:
-  chilaquiles = st.number_input("Manchego:", min_value=0, value=0)
-with c7:
-  chilaquiles = st.number_input("Huevo", min_value=0, value=0)
+for chilaquil in chilaquiles:
+  st.header(f"{chilaquil}")
+  c3, c4, c5, c6, c7 = st.columns([1, 1, 1, 1, 1])
+  with c3:
+    pedido = st.number_input("Plato:", min_value=0, value=0)
+  with c4:
+    chilaquiles = st.number_input("Torta:", min_value=0, value=0)
+  with c5:
+    chilaquiles = st.number_input("Jamón:", min_value=0, value=0)
+  with c6:
+    chilaquiles = st.number_input("Manchego:", min_value=0, value=0)
+  with c7:
+    chilaquiles = st.number_input("Huevo", min_value=0, value=0)
 
 
 
