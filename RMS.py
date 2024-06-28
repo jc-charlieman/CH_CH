366d6ec3aacb41db520112ab058ee6ef895cd21e
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-28 16:50:25 -0600

    Update RMS.py

--- RMS.py
+++ RMS.py
@@ -10,14 +10,23 @@ st.title("CHILAQUIL CHILANGO by Wisho!.")
 st.subheader("Menú:")
 
 # Comida
+# c1, c2 = st.columns([1, 3])
+# with c1:
+#   st.write("\n")
+#   st.write("\n")
+#   st.write("#Chilaquiles:")
+# with c2:
+#   chilaquiles = st.number_input("", min_value=0, value=0)
+
 c1, c2 = st.columns([1, 3])
 with c1:
-  st.write("\n")
-  st.write("\n")
-  st.write("Total de Chilaquiles:")
+  pedido = st.number_input("Pedido:", min_value=0, value=0)
 with c2:
-  chilaquiles = st.number_input("", min_value=0, value=0)
-  # chilaquiles = st.text_input("")
+  chilaquiles = st.number_input("Chilaquiles", min_value=0, value=0)
+
+
+# for chilaquil in chilaquiles:
+#   st.write("")
 
 
 
