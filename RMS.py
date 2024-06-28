7c652ca5def1267ed0c4fce5268f6c7c97262eed
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-28 15:37:45 -0600

    Add files via upload

---
+++ RMS.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import streamlit as sl
+import matplotlib.pyplot as plt
+from numpy import sin, cos, tan, arccos, arctan, deg2rad, rad2deg, sqrt
+import altair as alt
+
+# Sidebar
+sl.title("CHILAQUIL CHILANGO\n by Wisho!.")
+
+
+a1 = sl.number_input("Ch-1:")
+
