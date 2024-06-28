d9e000d54ac07aad0e82d82a17d46fb3e171dd9e
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-28 16:05:02 -0600

    Update RMS.py

--- RMS.py
+++ RMS.py
@@ -31,4 +31,22 @@ with col7:
 with col8:
   i4 = st.number_input("Ch-8", value=0)
 
+col9, col10, col11, col12 = st.columns([1, 1, 1, 1])
+with col9:
+  i1 = st.number_input("Ch-9:", value=0, min_value=0)
+with col10:
+  i2 = st.number_input("Ch-10", value=0)
+with col11:
+  i3 = st.number_input("Ch-11", value=0)
+with col12:
+  i4 = st.number_input("Ch-12", value=0)
 
+col13, col14, col15, col16 = st.columns([1, 1, 1, 1])
+with col13:
+  i1 = st.number_input("Beb-1:", value=0, min_value=0)
+with col14:
+  i2 = st.number_input("Beb-2", value=0)
+with col15:
+  i3 = st.number_input("Beb-3", value=0)
+with col16:
+  i4 = st.number_input("Beb-4", value=0)
