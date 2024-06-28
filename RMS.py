63ee4eb79a3e7dbd1c207a667333696a13e0f8a4
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-28 15:39:32 -0600

    Update RMS.py

--- RMS.py
+++ RMS.py
@@ -1,9 +1,9 @@
 
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 import streamlit as sl
-import matplotlib.pyplot as plt
-import altair as alt
+# import matplotlib.pyplot as plt
+# import altair as alt
 
 # Sidebar
 sl.title("CHILAQUIL CHILANGO\n by Wisho!.")
