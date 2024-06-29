227b8344e63aa9c0f849d554a8fd2036a456221c
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-29 12:45:16 -0600

    Update RMS.py

--- RMS.py
+++ RMS.py
@@ -105,7 +105,7 @@ for i in range(1, ch + 1):
     total += subtotal
     df0 = pd.DataFrame()
     df1 = pd.DataFrame(d)
-    df = concat([df0, df1])
+    df = pd.concat([df0, df1])
 
 st.write(df)
 st.write(total)
