ed9179bc20770ed996d58d59e10e38caef08a372
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-28 18:14:23 -0600

    Update RMS.py

--- RMS.py
+++ RMS.py
@@ -14,23 +14,24 @@ c1, c2 = st.columns([1, 1])
 with c1:
   pedido = st.number_input("\# Pedido:", min_value=0, value=0)
 with c2:
-  chilaquiles = st.number_input("\# Chilaquiles:", min_value=0, value=0)
+  ch = st.number_input("\# Chilaquiles:", min_value=0, value=0)
 
 total = 0
 # contenedores
-plato, torta, jamon, manchego, huevo = [], [0], [0], [0], [0]
-for i in range(chilaquiles):
+plato, torta, jamon, manchego, huevo = [0]*ch, [0]*ch, [0]*ch, [0]*ch, [0]*ch
+for i in range(ch):
   st.subheader(f"Chilaquil-{i + 1}")
-  c3, c4, c5, c6, c7 = st.columns([1, 1, 1, 1, 1])
-  with c3:
-    plato.append(st.number_input("Plato:", min_value=0, value=0))
-  with c4:
+  #cols = chilaquiles * 5
+  f"c{i+3}", f"c{i+4}", f"c{i+5}", f"c{i+6}", f"c{i+7}" = st.columns([1, 1, 1, 1, 1])
+  with f"c{i+3}":
+    plato[i] = (st.number_input("Plato:", min_value=0, value=0))
+  with f"c{i+4}:
     torta[i] = st.number_input("Torta:", min_value=0, value=0)
-  with c5:
+  with f"c{i+5}:
     jamon[i] = st.number_input("Jamón:", min_value=0, value=0)
-  with c6:
+  with f"c{i+6}:
     manchego[i] = st.number_input("Manchego:", min_value=0, value=0)
-  with c7:
+  with f"c{i+7}:
     huevo[i] = st.number_input("Huevo", min_value=0, value=0)
 
 st.write(plato)
