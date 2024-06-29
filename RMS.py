a3ce659b43f164558c3e75a2b359615436cf4905
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-29 12:26:14 -0600

    Update RMS.py

--- RMS.py
+++ RMS.py
@@ -98,8 +98,8 @@ for i in range(1, ch + 1):
     with c10:
       aguacate[i-1] = st.number_input("Aguacate", min_value=0, value=0, key=k[9])
 
-
-df = pd.DataFrame(d)
+df = pd.DataFrame()
+df pd.DataFrame.update(d)
 st.write(df)
 st.write(total)
 st.write(now)
