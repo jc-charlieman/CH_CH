a09df5ee6e9a10291601bdfafb16cd938fdba19f
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-29 12:47:15 -0600

    Update RMS.py

--- RMS.py
+++ RMS.py
@@ -112,10 +112,10 @@ for i in range(1, ch + 1):
 st.write(total)
 st.write(now)
 
-# @st.cache
-# def convert_csv(df):
-#   return df.to_csv(index=False).encode("utf-8")
+@st.cache
+def convert_csv(df):
+  return df.to_csv(index=False).encode("utf-8")
  
-# csv = convert_csv(df)
-# st.download_button(label="Download CSV", data=csv)
+csv = convert_csv(df)
+st.download_button(label="Download CSV", data=csv)
 
