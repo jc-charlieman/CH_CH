299c394a5b1fa70d660a5bbaf16f5f0f9baed24c
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-29 07:27:55 -0600

    Update RMS.py

--- RMS.py
+++ RMS.py
@@ -26,16 +26,20 @@ for i in range(1, ch + 1):
   #c = [f"c{ch * (col + 3)}" for col in range(5)]
   #c[0], c[1], c[2], c[3], c[4] = st.columns([1, 1, 1, 1, 1])
   c1, c2, c3, c4, c5 = st.columns([1, 1, 1, 1, 1])
+  if ch == 1:
+    k = [k for k in range(ch, ch + 5)]
+  else:
+    k = [k for k in range(5*ch, 5*ch - 5, -1)]]
   with c1:
-    plato[i-1] = (st.number_input("Plato:", min_value=0, value=0, key=1))
+    plato[i-1] = (st.number_input("Plato:", min_value=0, value=0, key=k[0]))
   with c2:
-    torta[i-1] = st.number_input("Torta:", min_value=0, value=0, key=2)
+    torta[i-1] = st.number_input("Torta:", min_value=0, value=0, key=k[1])
   with c3:
-    jamon[i-1] = st.number_input("Jamón:", min_value=0, value=0, key=3)
+    jamon[i-1] = st.number_input("Jamón:", min_value=0, value=0, key=k[2])
   with c4:
-    manchego[i-1] = st.number_input("Manchego:", min_value=0, value=0, key=4)
+    manchego[i-1] = st.number_input("Manchego:", min_value=0, value=0, key=k[3])
   with c5:
-    huevo[i-1] = st.number_input("Huevo", min_value=0, value=0, key=5)
+    huevo[i-1] = st.number_input("Huevo", min_value=0, value=0, key=k[4])
   # else:
   #   c = [f"c{col}" for col in range(5*ch, 5*ch - 5, -1)]
   #   c[0], c[1], c[2], c[3], c[4] = st.columns([1, 1, 1, 1, 1])
