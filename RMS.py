cc9230ec294e4c681a637ed756307b2df1a47534
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-29 07:09:40 -0600

    Update RMS.py

--- RMS.py
+++ RMS.py
@@ -19,35 +19,35 @@ total = 0
 # contenedores
 plato, torta, jamon, manchego, huevo = [0]*ch, [0]*ch, [0]*ch, [0]*ch, [0]*ch
 for i in range(1, ch + 1):
-  st.subheader(f"Chilaquil-{i + 1}")
+  st.subheader(f"Chilaquil-{i}")
   #cols = chilaquiles * 5
   #c = [f"c{ch * (col + 3)}" for col in range(5)]
   if i == 1:
     c = [f"c{ch * (col + 3)}" for col in range(5)]
     c[0], c[1], c[2], c[3], c[4] = st.columns([1, 1, 1, 1, 1])
     with c[0]:
-      plato[i] = (st.number_input("Plato:", min_value=0, value=0))
+      plato[i-1] = (st.number_input("Plato:", min_value=0, value=0))
     with c[1]:
-      torta[i] = st.number_input("Torta:", min_value=0, value=0)
+      torta[i-1] = st.number_input("Torta:", min_value=0, value=0)
     with c[2]:
-      jamon[i] = st.number_input("Jamón:", min_value=0, value=0)
+      jamon[i-1] = st.number_input("Jamón:", min_value=0, value=0)
     with c[3]:
-      manchego[i] = st.number_input("Manchego:", min_value=0, value=0)
+      manchego[i-1] = st.number_input("Manchego:", min_value=0, value=0)
     with c[4]:
-      huevo[i] = st.number_input("Huevo", min_value=0, value=0)
+      huevo[i-1] = st.number_input("Huevo", min_value=0, value=0)
   else:
     c = [f"c{col}" for col in range(5*ch, 5*ch - 5, -1)]
     c[0], c[1], c[2], c[3], c[4] = st.columns([1, 1, 1, 1, 1])
     with c[0]:
-      plato[i] = (st.number_input("Plato:", min_value=0, value=0))
+      plato[i-1] = (st.number_input("Plato:", min_value=0, value=0))
     with c[1]:
-      torta[i] = st.number_input("Torta:", min_value=0, value=0)
+      torta[i-1] = st.number_input("Torta:", min_value=0, value=0)
     with c[2]:
-      jamon[i] = st.number_input("Jamón:", min_value=0, value=0)
+      jamon[i-1] = st.number_input("Jamón:", min_value=0, value=0)
     with c[3]:
-      manchego[i] = st.number_input("Manchego:", min_value=0, value=0)
+      manchego[i-1] = st.number_input("Manchego:", min_value=0, value=0)
     with c[4]:
-      huevo[i] = st.number_input("Huevo", min_value=0, value=0)
+      huevo[i-1] = st.number_input("Huevo", min_value=0, value=0)
     
 st.write(plato)
 st.write(torta)
