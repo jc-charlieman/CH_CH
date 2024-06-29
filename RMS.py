477880d72ae2b5aea9d6ab1263a2390cf6e72b19
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-29 13:25:24 -0600

    Update RMS.py

--- RMS.py
+++ RMS.py
@@ -84,8 +84,20 @@ for i in range(1, ch + 1):
     k = [k for k in range(10*i, 10*i - 10, -1)]
     with c1:
       plato[i-1] = (st.number_input("Plato:", min_value=0, value=0, key=k[0]))
+      if plato[i-1] > 0:
+        d["orden"].append(orden)
+        d["articulo"].append(f"ch-{i}")
+        d["ingredientes"].append("plato")
+        d["volumen"].append(plato[i-1])
+        d["costo"].append(plato[i-1] * 65)  
     with c2:
       torta[i-1] = st.number_input("Torta:", min_value=0, value=0, key=k[1])
+      if torta[i-1] > 0:
+        d["orden"].append(orden)
+        d["articulo"].append(f"ch-{i}")
+        d["ingredientes"].append("torta")
+        d["volumen"].append(torta[i-1])
+        d["costo"].append(torta[i-1] * 60)
     with c3:
       jamon[i-1] = st.number_input("Jamón:", min_value=0, value=0, key=k[2])
     with c4:
